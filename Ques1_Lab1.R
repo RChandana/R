8f4dcfba9c744fc836f736ffe34ea09486e6e504
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2023-01-12 16:40:09 +0530

    Update Ques1_Lab1.R

--- Ques1_Lab1.R
+++ Ques1_Lab1.R
@@ -1,1 +1,11 @@
-
+# Enter the elements {5,12,7,2,6,45,11,3,63} and store in a variable 𝑥
+# Display 𝑥 values 
+# Find the number of elements of 𝑥
+# Find the 5th, 8th elements
+# Find the minimum element of 𝑥.
+# Find the maximum element of 𝑥.
+# Enter the data {1, 2, .... ,19,20} in a variable 𝑥.
+# Find the 3rd element in the data list.
+# Find 3rd to 5th element in the data list.
+# Find 2nd, 5th, 6th, and 12th element in the list.
+# Print the data as {20, 19, ..., 2, 1} without again entering the data.
