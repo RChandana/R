d537914a80a5067343b878337b62884cb698be71
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2023-01-12 16:44:40 +0530

    Update Ques2_Lab1.R

--- Ques2_Lab1.R
+++ Ques2_Lab1.R
@@ -1,1 +1,9 @@
-
+# Reading a data file and working with it: 
+# a) Read the file first and store it in a.
+# b) How many rows are there in this table? How many columns are there? 
+# c) How to find the number of rows and number of columns by a single command?
+# d) What are the variables in the data file?
+# e) If the file is very large, naturally we cannot simply type `a', because it will cover the entire screen and we won't be able to understand anything. 
+#    So how to see the top or bottom few lines in this file?
+# f) If the number of columns is too large, again we may face the same problem. So how to see the first 5 rows and first 3 columns?
+# g) How to get 1st, 3rd, 6th, and 10th row and 2nd, 4th, and 5th column? h) How to get values in a specific row or a column?
