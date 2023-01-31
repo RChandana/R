c66c9c479db519a29d25b1812c6fac713258ca29
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2023-01-31 15:42:36 +0530

    Update Ques2_Lab2.R

--- Ques2_Lab2.R
+++ Ques2_Lab2.R
@@ -8,3 +8,11 @@ cat("Median of Happiness = ", median(data$happiness))
 
 cat("Standard Deviation of Income = ", sd(data$income))
 cat("Standard Deviation of Happiness = ", sd(data$happiness))
+
+par(mfrow=c(2,2))
+
+plot(data$income, type = "l", income = "Income vs Happiness", xlab = "Happiness", ylab = "Income", col = "pink")
+
+hist(data$income, type = "hist", income = "Histogram", xlab = "Happiness", ylab = "Income", col = "purple")
+
+boxplot(data$income, type = "boxgraph", income = "Box Graph", xlab = "Happiness", ylab = "Income", col = "red")
