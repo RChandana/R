828518529340268f31acba00e9efd693c9bb9719
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2023-01-31 15:43:03 +0530

    Update Ques1_Lab2.R

--- Ques1_Lab2.R
+++ Ques1_Lab2.R
@@ -1,1 +1,9 @@
-
+data = 1 : 10
+data
+sum(1:10)
+mean(1:10)
+median(1:10)
+cat("Sum of squares = ", sum(data*data))
+cat("Mean deviation about Mean = ", mean(abs(data-mean(data))))
+cat("Checking if MD x is less than or equal to tandard deviation = ", mean(abs(data - mean(data))) <= sd(data))
+cat("Standard Deviation = ", sqrt(sum((data - mean(data))^2) / length(data)))
