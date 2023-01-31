ab39c29aeb5414d0eab2c759bfd4b5b98c6a2672
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2023-01-31 15:42:23 +0530

    Create Ques2_Lab2.R

--- Ques2_Lab2.R
+++ Ques2_Lab2.R
@@ -1,1 +1,10 @@
+data = read.csv("Downloads/income.data.csv")
+print(head(data))
+cat("Mean of Income = ", mean(data$income))
+cat("Mean of Happiness = ", mean(data$happiness))
 
+cat("Median of Income = ", median(data$income))
+cat("Median of Happiness = ", median(data$happiness))
+
+cat("Standard Deviation of Income = ", sd(data$income))
+cat("Standard Deviation of Happiness = ", sd(data$happiness))
